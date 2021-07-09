f71967e8f8983802f26ef10692f089fccf39c873
Joseph Ao <36581618+killerfrost22@users.noreply.github.com>
2021-07-09 13:09:53 -0400

    updates

---
+++ src/practice/test.js
@@ -0,0 +1,9 @@
+function datescounting(a, b) {
+    var result = 0;
+    for (var i = 0; i < a.length; i++) {
+        if (a[i] < b[i]) {
+            result++;
+        }
+    }
+    return result;
+}
